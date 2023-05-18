d4d495daa12dca4137515cf46e86739661ab72fa
Sanunatta <128024975+Sanunatta@users.noreply.github.com>
2023-05-18 12:58:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const weathericon = document.querySelector('.weather-img');
 
 async function checkWeather(city){
     const api_key = "3eaaa12de85207b48fd34d3e53767c63";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;  const apikey="195cc261-9089-48a5-b9f5-0589755fdba5";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`; 
     const apiUrl="https://api.meteum.ai/v1/forecast?lat=52.37125&lon=4.89388";
 
     async function cheackWeather(city){
@@ -25,19 +25,19 @@ async function checkWeather(city){
         document.querySelector('wind-speed').innerHTML=data.wind.speed+"km/hr";
 
         if(data.wether[0].main == "Clouds"){
-            weathericon.src="images/clouds.png"
+            weathericon.src="clouds.png"
         }
         else if(data.weather[0].main == "Clear"){
-            weathericon.src="images/clear.png"
+            weathericon.src="clear.png"
         }
         else if(data.weather[0].main == "Rain"){
-            weathericon.src="images/rain.png"
+            weathericon.src="/rain.png"
         }
        else if(data.weather[0].main == "Drizzle"){
-            weathericon.src="images/drizzle.png"
+            weathericon.src="drizzle.png"
         }
         else if(data.weather[0].main == "Mist"){
-            weathericon.src="images/mist.png"
+            weathericon.src="mist.png"
         }
         document.querySelector('.weather-body').style.display="block";
         document.querySelector('.location-not-found').style.display="none";
